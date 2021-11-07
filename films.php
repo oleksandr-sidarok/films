3012b9b59197eccd02b20a3fddb1826c153c6b43
Oleksandr Sidarok <sasha.sidarok@disroot.org>
2021-11-07 15:38:45 +0200

    add file films.php

---
+++ films.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ua">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="./css/main.css">
+  <title>Films</title>
+</head>
+<body>
+  <header class="header">
+    <a href="#" class="logo">Films</a>
+    <div class="search__wrapper">
+      <input class="header__search"></input>
+      <button class="header__btn"></button>
+    </div>
+  </header>
+  <main>
+    <section>
+      <img src="img/background/wonder-woman.jpg" alt="">
+      <div>
+        <img src="img/cover/wonder-woman.jpg" alt="">
+      </div>
+    </section>
+  </main>
+</body>
+</html>
